0ac6db316fbd326b28e4c60f2f107dee867ab00b
Seb Gertz <sebgertz@gmail.com>
2018-05-11 18:42:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function BoardMember(name, homeState, training) {
   this.veto = function() {return "No, I must disagree"};
   this.approve = function(){return 'You can do this!'};
   this.doCharity = function() {return "I like to help people."};
-  this.
+  this.releasePressStatement = function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 
 }
